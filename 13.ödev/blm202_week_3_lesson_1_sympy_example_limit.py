c26db44cb412b8117226201ac654e249db6c6b3a
Ozgur Kucet <ozgurkucet@hotmail.com>
2020-04-16 22:05:19 +0300

    ödevler

---
+++ 13.ödev/blm202_week_3_lesson_1_sympy_example_limit.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 20:26:27 2020
+
+@author: Ozgur Kucet
+"""
+from sympy import pprint
+from sympy import Symbol, Limit 
+t = Symbol('t') 
+St = 5*t**2 + 2*t + 8
+
+t1 = Symbol('t1')
+delta_t = Symbol('delta_t')
+
+St1 = St.subs({t: t1})
+St1_delta = St.subs({t: t1 + delta_t})
+
+
+pprint((St1_delta-St1)/delta_t)
+print("Limiti alınca:")
+pprint(Limit((St1_delta-St1)/delta_t, delta_t, 0).doit())#fonksyonun limiti bulunur.
---
+++ 13.ödev/blm202_week_3_lesson_2_sympy_integral_pdf_example.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 20:37:35 2020
+
+@author: Ozgur Kucet
+"""
+
+
+"""
+P(11<x<12)=n(E)/n(S)
+ 
+"""
+#E, 11 ile 12 arasında mümkün olan tüm sınıfların kümesidir
+#S, olası tüm sınıfların kümesidir—yani, 1 ile 20 arasındaki tüm gerçek sayılar.
+#P(x) a probability density function
+
+"""
+n(E) sonsuzdur, çünkü olası tüm gerçek sayıları 11 ile 12
+arasında saymak imkansızdır; aynı şey n (ler) için de geçerlidir.
+Bu nedenle, olasılığı hesaplamak için farklı bir yaklaşıma ihtiyacımız var.
+"""
+
+from sympy import pprint
+
+from sympy import Symbol, exp, sqrt, pi, Integral 
+x = Symbol('x') 
+p = exp(-(x - 10)**2/2)/sqrt(2*pi)
+pprint(p)
+
+pprint(Integral(p, (x, 11, 12)).doit().evalf())#11 ile 12 arasında olma olasılığı
+
+
+
